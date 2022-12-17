9fe685ea002998ea5644d15a227c5ec9fe0519cf
pabloortegaa <63413550+pabloortegaa@users.noreply.github.com>
2022-12-17 18:46:28 +0100

    checks

--- backend_api/__init__.py
+++ backend_api/__init__.py
@@ -1,21 +1,33 @@
 from flask import Flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pablo@localhost/test'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pa$$w0rd1990@jseijas-dbsrv/portega-assignment-db-prod'
+load_dotenv()
+
+# Select environment based on the ENV environment variable
+if os.getenv('ENV') == 'dev':
+    print("Running in development mode")
+    app.config.from_object('config.DevelopmentConfig')
+elif os.getenv('ENV') == 'ghci':
+    print("Running in github mode")
+    app.config.from_object('config.GithubCIConfig')
+else:
+    print("Running in production mode")
+    app.config.from_object('config.ProductionConfig')
+
+
 db = SQLAlchemy(app)
 
 from backend_api.models import Recipe
-from backend_api import routes
-
 db.create_all()
-
-from flask_cors import CORS
 CORS(app)
 
+from backend_api import routes
 
 
 
